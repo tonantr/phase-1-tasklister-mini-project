c93360f20127b2ddc3b5efeecb90ac44f907d73a
tonantr <tonantruong@hotmail.com>
2023-04-12 17:38:53 -0400

    complete Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+const form = document.querySelector('#create-task-form');
+const taskUl = document.querySelector('#tasks');
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const user = e.target.user.value;
+    const tasks = e.target['new-task-description'].value;
+    const obj = { user, tasks };
+    displayList(obj);
+    form.reset();
+  });
 });
+
+function displayList(params) {
+  const span = document.createElement('span');
+  const li = document.createElement('li');
+  const delBtn = document.createElement('button');
+
+  span.textContent = `${params.user} - ${params.tasks}`;
+  delBtn.textContent = 'X';
+  delBtn.addEventListener('click', handleDel);
+
+  span.appendChild(delBtn);
+  li.appendChild(span);
+  taskUl.appendChild(li);
+
+};
+
+function handleDel(e) {
+  e.stopPropagation();
+  e.target.parentNode.parentNode.remove();
+};
+
